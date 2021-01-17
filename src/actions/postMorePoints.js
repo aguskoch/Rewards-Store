a7ef222422780d870dc0920047bad8b101757c6b
Agustina koch <agustinapkoch@gmail.com>
2021-01-17 19:42:57 -0300

    Set Modal for "Success Adding more Coins"

--- src/actions/postMorePoints.js
+++ src/actions/postMorePoints.js
@@ -5,11 +5,8 @@ async function postMoreCoins(amount) {
     
 
     try {
-        console.log(amount)
         const response = await fetch(URL_POSTCOINS, { method: "POST", headers, body: JSON.stringify({"amount": amount}) })
-        console.log(response, amount)
         const data = await response.json()
-        console.log(data, amount)
         return data
     } catch (error) {
         console.log("Error", error)
--- src/components/addCoins.js
+++ src/components/addCoins.js
@@ -1,26 +1,42 @@
 import React from "react"
 import coin from "../assets/coin.svg"
 import postMoreCoins from "../actions/postMorePoints"
-import { THOUSAND, FIVETHOUSAND, SEVENTHOUSAND} from "../actions/constants"
+import { THOUSAND, FIVETHOUSAND, SEVENTHOUSAND } from "../actions/constants"
 
-function AddCoins() {
+
+function AddCoins(props) {
+
+    const [ messageSuccessCoins, setMessageSuccessCoins] = React.useState("")
+
+    const handleClickCoins = (number) =>{
+        postMoreCoins(THOUSAND).then(response => setMessageSuccessCoins(response.message))
+    }
+
+
     return (
-        <div>
-            <h4> Add more coins </h4>
-            <button onClick= {() => postMoreCoins(THOUSAND)}>
-                <img src={coin} alt={THOUSAND} />
-                {THOUSAND}
-            </button>
-            <button onClick= {() => postMoreCoins(FIVETHOUSAND)}>
-                <img src={coin} alt={FIVETHOUSAND} />
-                {FIVETHOUSAND}
-            </button>
-            <button onClick= {() => postMoreCoins(SEVENTHOUSAND)}>
-                <img src={coin} alt={SEVENTHOUSAND}  />
-                {SEVENTHOUSAND}
-            </button>
+        <div className="modal-screen">
+            <section className="addCoins">
+                <div className={messageSuccessCoins === "Points Updated" ? "successEnabled" : "successDisabled"}> 
+                    <h1> Success </h1>
+                    <button onClick={() => setMessageSuccessCoins("")}> X </button>
+                </div>
+                <h4 className="title-addCoins"> Add more coins </h4>
+                <button className="button-addCoins one" onClick={() => handleClickCoins(THOUSAND)}>
+                    <img src={coin} alt={THOUSAND} />
+                    {THOUSAND}
+                </button>
+                <button className="button-addCoins two" onClick={() => handleClickCoins(FIVETHOUSAND)}>
+                    <img src={coin} alt={FIVETHOUSAND} />
+                    {FIVETHOUSAND}
+                </button>
+                <button className="button-addCoins three" onClick={() => handleClickCoins(SEVENTHOUSAND)}>
+                    <img src={coin} alt={SEVENTHOUSAND} />
+                    {SEVENTHOUSAND}
+                </button>
+                <button className="closeAddCoins" onClick={() => props.setModal(false)}> X </button>
+            </section>
         </div>
-
+        
 
     )
 }
